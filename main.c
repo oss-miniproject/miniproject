699c1d2b0a25abaeb7ca2aa2c00273559605f6c5
seongju1015 <122343583+seongju1015@users.noreply.github.com>
2023-05-16 02:05:46 +0900

    Update manager mode main

--- main.c
+++ main.c
@@ -13,7 +13,43 @@ int main() {
         if (mode == 1) {
             manager_mode = manager_selectMenu();
             if(manager_mode == 0){
+                saveData(u_list, index);
+                printf("\n로그아웃 되었습니다!\n");
                 break;
+            }
+            if(user_mode == 1 || user_mode == 3 || user_mode == 4){
+                if(count == 0){
+                    printf("입력된 정보가 없습니다.\n ");
+                        continue;
+                }
+            }
+            else if (user_mode == 2) {
+                if(addUser(u_list,count) == 0){
+                    continue;
+                }
+                else{
+                    count++;
+                    printf("=> 추가됨\n");
+                }
+            }
+            else if (user_mode == 3){
+                if(updateUser(u_list,count) == 0){
+                    continue;
+                }
+                else{
+                    printf("=> 수정됨\n");
+                }
+            }
+            else if (user_mode == 4){
+                if(deleteUser(u_list,count) == 0){
+                    continue;
+                }
+                else{
+                    printf("=> 삭제됨\n");
+                }
+            }
+            else if(user_mode == 5){
+                searchData(u_list,count);
             }
         }
         else if (mode == 2) {
